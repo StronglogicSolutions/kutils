3e263a13b50c60b3cc4a42bb6b87377f426833de
Emmanuel Buckshi <adventurist@gmail.com>
2023-11-04 13:31:28 -0400

    adding token bucket implementation

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char* argv[])
 {
   kutils::kargs args{argc, argv};
   kutils::log("loglevel is: ", args.get("loglevel").c_str());
+  kutils::bucket<4, 60> bucket;
 
   return 0;
 }
